9c430cb154cc5dc1b130ab000f9b75662fc9e697
George Hughey <ghughey@umd.edu>
2016-02-04 11:32:51 -0800

    Changed for individual cells.

--- script.py
+++ script.py
@@ -6,30 +6,50 @@ import json
 from oauth2client.client import SignedJwtAssertionCredentials
 import gspread 
 
-json_key = json.load(open('hacs.json'))
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+def start():
+    json_key = json.load(open('hacs.json'))
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 
-gc = gspread.authorize(credentials)
+    gc = gspread.authorize(credentials)
+    return gc
 
-spread = gc.open_by_key("1QXFTpHJVI79nLunRp8FJFSufMnjTl6NmW1WM7EAiy4c").sheet1
 
-cell = "B2"
-startcell=2
 
-os.system("echo \"\" > /home/george/ssh")
-os.system("echo \"\" > /home/george/ssh")
 
-while(True):#care: grepping for fbuary as a temporary fix to remove the difference header.
-    os.system("cat /var/log/auth.log | grep \"sshd:session): session opened\" | grep -v \"grep\" > /home/george/tmp")
-    proc = subprocess.Popen(["diff /home/george/tmp /home/george/ssh | grep \"Feb\""], stdout=subprocess.PIPE, shell=True)
-    difference = proc.stdout.read()
-    if difference != "":
-	entry = spread.update_acell(cell, difference)
-	os.system("cat /home/george/tmp > /home/george/ssh")
-	startcell = startcell+1
-	cell=str("B") + str(startcell)
+def main():
 
-    os.system("echo \"\" > ~/tmp")
-    time.sleep(3)
+    auth = start()
+    
+    spread = auth.open_by_key("1QXFTpHJVI79nLunRp8FJFSufMnjTl6NmW1WM7EAiy4c").sheet1
+    
+    startcell=2
+
+    os.system("echo \"\" > /home/george/ssh")
+    os.system("echo \"\" > /home/george/ssh")
+    
+    while(True):
+        os.system("cat /var/log/auth.log | grep \"sshd:session): session opened\" | grep -v \"grep\" > /home/george/tmp")
+        proc = subprocess.Popen(["diff /home/george/tmp /home/george/ssh | grep \"Feb\""], stdout=subprocess.PIPE, shell=True)
+        difference = proc.stdout.read()
+        if difference != "":
+            lines = difference.split("\n")
+            while len(lines) > 0:
+        	
+                addline = lines.pop(0)
+                if(addline == ""):
+                    break;
+                
+                entry = spread.update_acell(str("B")+str(startcell), (addline[:18])[2:])
+                processentry = spread.update_acell(str("C")+str(startcell), (addline.split("[")[1].split("]")[0]))
+                userentry = spread.update_acell(str("D")+str(startcell), (addline.split("for user ")[1].split(" ")[0]))
+                os.system("cat /home/george/tmp > /home/george/ssh")
+        	startcell = startcell+1
+
+        os.system("echo \"\" > ~/tmp")
+        time.sleep(3)
+
+
+main()
+
 
